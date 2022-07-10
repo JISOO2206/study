776ab7a9e2210cc0f4cd1406ca0ffe480a88ea73
JISOO2206 <900522z@gmail.com>
2022-07-11 00:39:45 +0900

    blushColorChange

--- Js/painting Board/app.js
+++ Js/painting Board/app.js
@@ -1,4 +1,12 @@
 const canvas = document.getElementById("jsCanvas");
+const context = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+canvas.width = 500;
+canvas.height = 500;
+
+context.strokeStyle = "#2c2c2c";
+context.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,22 +14,36 @@ function stopPainting(){
   painting = false;
 }
 
+function startPainting(){
+  painting = true;
+}
+
 function onMouseMove(event){
   const x = event.offsetX;
   const y = event.offsetY;
+  if(!painting){
+    context.beginPath();
+    context.moveTo(x,y);
+  }else{
+    context.lineTo(x,y);
+    context.stroke();
+  }
 }
 
 function onMouseDown(event){
   painting = true;
 }
 
-function onMouseUp(){
-  
+function onClickChangeColor(event){
+  const thisColor = event.target.style.backgroundColor;
+  context.strokeStyle = thisColor;
 }
 
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click",onClickChangeColor))
