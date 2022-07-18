fca5d7320ecf64f1b7fac0cbd07c53a4b02c70e4
JISOO2206 <900522z@gmail.com>
2022-07-18 22:05:18 +0900

    브러쉬 크기 설정

--- Js/painting Board/app.js
+++ Js/painting Board/app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const context = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const fillBtn = document.getElementById('jsMode');
 
 canvas.width = 500;
 canvas.height = 500;
@@ -9,6 +11,7 @@ context.strokeStyle = "#2c2c2c";
 context.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
   painting = false;
@@ -39,6 +42,21 @@ function onClickChangeColor(event){
   context.strokeStyle = thisColor;
 }
 
+function handleRangeChange(event){
+  const size = event.target.value;
+  context.lineWidth = size;
+}
+
+function handleModeClick(event){
+  if(filling === true){
+    filling = false;
+    fillBtn.innerText = "Fill"
+  }else{
+    filling = true;
+    fillBtn.innerText = "Paint"
+  }
+}
+
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
@@ -46,4 +64,12 @@ if(canvas){
   canvas.addEventListener("mouseleave", stopPainting);
 }
 
-Array.from(colors).forEach(color => color.addEventListener("click",onClickChangeColor))
+Array.from(colors).forEach(color => color.addEventListener("click",onClickChangeColor));
+
+if(range){
+  range.addEventListener('input', handleRangeChange)
+}
+
+if(fillBtn){
+  fillBtn.addEventListener('click', handleModeClick)
+}
