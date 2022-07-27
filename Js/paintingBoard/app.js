873b44ed8c2e160f0de370247514adcf44fc8772
JISOO2206 <900522z@gmail.com>
2022-07-27 23:32:36 +0900

    moveTo, lineTo, Stroke를 이용해 사각형 그리기

--- Js/paintingBoard/app.js
+++ Js/paintingBoard/app.js
@@ -5,18 +5,26 @@ canvas.width = 800;
 canvas.height = 800;
 
 // 사각 도형을 그릴 수 있는 함수, (단, fill함수를 이후 라인에 사용하지 않으면 도형은 보이지 않는다.)
-context.rect(50, 50, 100, 100);
-context.rect(150, 150, 100, 100);
-context.rect(250, 250, 100, 100);
+// 그리고 바로 보이는 걸 원한다면 fillRect 사용
+// context.rect(50, 50, 100, 100);
+// context.rect(150, 150, 100, 100);
+// context.rect(250, 250, 100, 100);
 
-// 도형에 색을 채우는 함수.
-context.fill();
+// // 도형에 색을 채우는 함수.
+// context.fill();
 
-// 상단 도형과 경로(적용)를 분리시킬 때 사용
-context.beginPath();
-context.rect(350, 350, 100, 100);
-// 도형의 색상을 변경하는 프로퍼티
-context.fillStyle = 'blue';
+// // 상단 도형과 경로(적용)를 분리시킬 때 사용
+// context.beginPath();
+// context.rect(350, 350, 100, 100);
+// // 도형의 색상을 변경하는 프로퍼티
+// context.fillStyle = 'blue';
+
+context.moveTo(50,50);
+context.lineTo(150, 50)
+context.lineTo(150, 150)
+context.lineTo(50, 150)
+context.lineTo(50, 50)
+context.stroke();
 context.fill();
 
 
