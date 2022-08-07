e90f61dde806365a50eb6b091ae0bf1f69f1985a
JISOO2206 <900522z@gmail.com>
2022-08-08 00:40:19 +0900

    form 마크업 및 이벤트 발생으로 인한 데이터 통신

--- Vue/vue-form/src/App.vue
+++ Vue/vue-form/src/App.vue
@@ -1,10 +1,46 @@
 <template>
-  <div></div>
+  <!-- 바닐라 js로 새고를 막던 것을 vue에서는 이벤트명.prevent로 막을 수 있다 -->
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="userName">id : </label>
+      <input id="userName" type="text" v-model="userName" />
+    </div>
+    <div>
+      <label for="password">password : </label>
+      <input id="password" type="password" v-model="password" />
+    </div>
+    <button type="submit">login</button>
+  </form>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data: function(){
+    return {
+      userName : '',
+      password : ''
+    }
+  },
+  methods: {
+    submitForm(){
+      //sub으로 인한 새로고침을 막아주는 이벤트(바닐라 자바스크립트)
+      // event.preventDefault();
+      let url = 'https://jsonplaceholder.typicode.com/users';
+      let data = {
+        userName: this.userName,
+        password : this.password
+      }
+      axios.post(url, data)
+        .then(function (responese) {
+          console.log(responese)
+        })
+        .catch(function (error) {
+          console.log(error)
+        });
+    }
+  }
 }
 </script>
 
