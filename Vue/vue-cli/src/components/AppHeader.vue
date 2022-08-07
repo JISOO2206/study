c361d7cdbca49c71d78a52b0aece8e83031848fc
JISOO2206 <900522z@gmail.com>
2022-08-07 23:46:06 +0900

    emit 학습

---
+++ Vue/vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>{{propsData}}</h1>
+    <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+<script>
+export default {
+  props: ['propsData'],
+  methods: {
+    sendEvent(){
+      this.$emit('renew');
+    }
+  },
+}
+</script>
+<style>
+  
+</style>
