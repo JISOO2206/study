1c29665970de538cad2faa76d7a1e30b9f151ed7
JISOO2206 <900522z@gmail.com>
2022-08-07 23:48:10 +0900

    변경 commit

--- Vue/vue-cli/src/App.vue
+++ Vue/vue-cli/src/App.vue
@@ -1,31 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <hello-world></hello-world>
+  <div>
+  <app-header v-bind:propsData="str"
+              v-on:renew="renewStr"></app-header>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
 
 export default {
-  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: 'App',
-  components: {
-    HelloWorld,
-    'hello-world': HelloWorld,
-  }
+  data() {
+    return {
+      str: 'HHeader'
+    }
+  },
+  components:{
+    'app-header' : AppHeader
+  },
+  methods: {
+    renewStr(){
+      this.str = 'hi';
+    }
+  },
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  
 </style>
