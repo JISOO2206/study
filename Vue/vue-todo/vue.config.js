236ab981c325ff94427a3f455fa7325f62c386f5
JISOO2206 <900522z@gmail.com>
2022-08-11 23:01:15 +0900

    vue 초기 환경설정 파일

--- Vue/vue-todo/vue.config.js
+++ Vue/vue-todo/vue.config.js
@@ -2,3 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+const path = require('path');
+
+module.exports = {
+  chainWebpack : config => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src/'));
+    config.plugin('html').tab(args => {
+      args[0].template = '@/index.html'
+      return args
+    })
+  }
+}
