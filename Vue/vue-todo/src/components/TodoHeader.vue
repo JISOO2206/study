741c890d62d31b1adf7e9af20a08520e933ebdce
JISOO2206 <900522z@gmail.com>
2022-08-11 23:00:32 +0900

    vue components

---
+++ Vue/vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1 class="title">TODO it!</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.title{
+  color: cornflowerblue !important;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
