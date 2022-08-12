cf6070580c4f56a2f10aaf115818c928a2c7f370
JISOO2206 <900522z@gmail.com>
2022-08-13 01:46:48 +0900

    주석 추가

--- Vue/vue-todo/src/components/TodoHeader.vue
+++ Vue/vue-todo/src/components/TodoHeader.vue
@@ -10,9 +10,10 @@ export default {
 }
 </script>
 
-<style>
+<!-- scoped => 해당 컴포넌트에서만 유효한 style을 줄 때 사용 -->
+<style scoped>
 .title{
-  color: cornflowerblue !important;
+  color: cornflowerblue;
   margin: 2.5rem 0 1.5rem;
 }
 </style>
