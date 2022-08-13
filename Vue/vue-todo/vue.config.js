16ac3915ee253b2267057b7c4c245642b9777657
JISOO2206 <900522z@gmail.com>
2022-08-13 15:33:27 +0900

    scr/index.html 제거로 인한 로직 제거

--- Vue/vue-todo/vue.config.js
+++ Vue/vue-todo/vue.config.js
@@ -3,15 +3,13 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
+//public 폴더가 아닌 다른 경로의 index.html 파일로 지정하는 방법
+
 const path = require('path');
 
 module.exports = {
   chainWebpack : config => {
     config.resolve.alias
       .set('@', path.resolve(__dirname, 'src/'));
-    config.plugin('html').tab(args => {
-      args[0].template = '@/index.html'
-      return args
-    })
   }
 }
