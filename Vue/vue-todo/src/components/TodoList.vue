b815466dc9408a6fc376a6b7b2e35261659fbe2c
JISOO2206 <900522z@gmail.com>
2022-08-13 20:52:52 +0900

    todoList 구현

--- Vue/vue-todo/src/components/TodoList.vue
+++ Vue/vue-todo/src/components/TodoList.vue
@@ -1,15 +1,91 @@
 <template>
   <div>
-    
+    <ul class="listBox">
+      <li class="listItem shadow" v-for="(todoItem,index) in todoArr" v-bind:key="index">
+        <span class="checkBtn" v-bind:class="{checkBtnCompleted: todoItem.completed}" @click="toggleComplete(todoItem)">
+          <i class="fa-solid fa-check"></i>
+        </span>
+        <span class="todoContent" v-bind:class="{todoCompleted : todoItem.completed}">{{todoItem.item}}</span>
+        <button class="deleteBtn" @click="deleteTodo(item, index)">
+          <i class="fa-solid fa-trash-can fa-lg"></i>
+        </button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return{
+      todoArr : []
+    }
+  },
+  //인스턴스가 생성되면 실행되는 라이프 사이클, 1번만 실행됨
+  created(){
+    if(localStorage.length > 0){
+      console.log(localStorage)
+      for(let i = 0; i<localStorage.length; i++){
+        this.todoArr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        // this.todoArr.push(localStorage.value())
+      }
+    }
+  },
+  methods: {
+    deleteTodo(todoItem, index){
+      localStorage.removeItem(todoItem);
+      this.todoArr.splice(index, 1);
+    },
+    toggleComplete(todoItem){
+      todoItem.completed = !todoItem.completed;
+      //로컬 스토리지의 데이터를 갱신해야 할까?
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    }
+  },
 }
 </script>
 
-<style>
+<style scoped>
 
+
+.listBox{
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0px;
+  text-align: left;
+}
+.listItem{
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  background: white;
+  border-radius: 5px;
+}
+.listItem .todoContent{
+  display: block;
+  padding-left: 20px;
+  width: 75%;
+}
+.checkBtn{
+  line-height: 45px;
+  color: #62acde;
+  display: block;
+  padding-left: 20px;
+}
+.checkBtnCompleted{
+  color: #b3adad;
+}
+.deleteBtn{
+  margin: auto;
+  color: tomato;
+  border: none;
+  background: none;
+}
+.todoCompleted{
+  text-decoration: line-through;
+  color: #b3adad;
+}
 </style>
