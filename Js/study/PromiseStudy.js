124c418042c78bfc5be5318660466c2b2c97f746
JISOO2206 <900522z@gmail.com>
2022-09-25 23:44:10 +0900

    promise 동작방식

---
+++ Js/study/PromiseStudy.js
@@ -0,0 +1,19 @@
+/**
+ * promise 기초
+ */
+
+function getData(callback) {
+  // new Promise() 추가
+  return new Promise(function(resolve, reject) {
+    $.get('url 주소/products/1', function(response) {
+      // 데이터를 받으면 resolve() 호출
+      resolve(response);
+    });
+  });
+}
+
+// getData()의 실행이 끝나면(성공시) 호출되는 then()
+getData().then(function(tableData) {
+  // resolve()의 결과 값이 여기로 전달됨
+  console.log(tableData); // $.get()의 reponse 값이 tableData에 전달됨
+});
