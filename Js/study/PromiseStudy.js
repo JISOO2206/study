7d03dcf4b6118c903bcdaf29d572b19b5117a1ed
JISOO2206 <900522z@gmail.com>
2022-10-03 11:03:12 +0900

    연속적인 promise 실행 순서

--- Js/study/PromiseStudy.js
+++ Js/study/PromiseStudy.js
@@ -35,6 +35,32 @@ function rejectGetData() {
 }
 
 // reject()의 결과 값 Error를 err에 받음
+// 실패 상태가 되면 실패한 이유룰 catch로 받을 수 있음
 rejectGetData().then().catch(function(err) {
   console.log(err); // Error: Request is failed
+});
+
+/**
+ * 여러 개의 프로미스 연결하기 (promise Chaining)
+ */
+
+new Promise(function(resolve, reject){
+  setTimeout(function() {
+    // 50번째 라인의 resolve가 호출되면 대기 -> 이행 상태로 넘어감
+    resolve(1);
+  }, 2000);
+})
+.then(function(result) {
+  //resolve가 이행상태로 넘어가면서 1번째 then 실행
+  console.log(result); // 1
+  return result + 10;
+})
+.then(function(result) {
+  //1번째 then의 실행 완료하고 그 return 값을 받아 2번째 then 실행
+  console.log(result); // 11
+  return result + 20;
+})
+.then(function(result) {
+  //2번째 then의 실행 완료하고 그 return 값을 받아 3번째 then 실행
+  console.log(result); // 31
 });
