73479f273cc0c5c47c14c9ddac0a04c7691261e4
JISOO2206 <900522z@gmail.com>
2022-11-05 12:27:42 +0900

    실제 값들을 env 환경변수 값으로 변경

--- React/whisper/src/firebase.js
+++ React/whisper/src/firebase.js
@@ -2,12 +2,12 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDN3rdOvICViQb_CBKJGk7cja6Z-Y5ZqfY",
-  authDomain: "whisper-dccc5.firebaseapp.com",
-  projectId: "whisper-dccc5",
-  storageBucket: "whisper-dccc5.appspot.com",
-  messagingSenderId: "320306458932",
-  appId: "1:320306458932:web:d27380893b5e6d1ebef561"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
