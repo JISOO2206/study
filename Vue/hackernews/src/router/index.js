1ac5c775707752608de6d2c5c6dc46087bbd1daf
JISOO2206 <900522z@gmail.com>
2023-01-29 21:13:48 +0900

    vue 파일 수정항목 commit 230129

--- Vue/hackernews/src/router/index.js
+++ Vue/hackernews/src/router/index.js
@@ -1,23 +1,33 @@
-import { createApp } from 'vue'
-import VueRouter from 'vue-router'
+import { createWebHashHistory, createRouter } from "vue-router";
+import AskView from '@/views/AskView.vue'
+import JobsView from '@/views/JobsView.vue'
+import NewsView from '@/views/NewsView.vue'
 
-createApp.use(VueRouter)
+const routes = [
+  {
+    // url 경로
+    path : "/askView",
+    // 해당 경로로 연결할 때 사용할 선택적 이름
+    // (근데 아직 용도에 대해서 감이 안 잡힘...)
+    name : "AskView",
+    // 해당 경로로 호출할 때 로드되는 구성요소
+    component : AskView
+  },
+  {
+    path : "/jobsView",
+    name : "JobsView",
+    component : JobsView
+  },
+  {
+    path : "/newsView",
+    name : "NewsView",
+    component : NewsView
+  }
+];
 
-const router = new VueRouter({
-  routes : [
-    {
-      // path : url에 대한 정보가 담기는 곳.(url 주소)
-      path : '',
-      // component : url 주소로 갔을 때 표시될 컴포넌트
-      component: ''
-    },
-    {
-      path : '',
-      component: ''
-    },
-    {
-      path : '',
-      component: ''
-    },
-  ]
-})
+const router = createRouter({
+  history : createWebHashHistory(),
+  routes
+});
+
+export default router;
--- Vue/hackernews/src/views/AskView.vue
+++ Vue/hackernews/src/views/AskView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-
+    ask
   </div>
 </template>
 
