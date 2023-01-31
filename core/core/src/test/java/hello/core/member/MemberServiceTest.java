fb0a5215fdac78b7a1af26131a3ca3c3ad4ec9ee
JISOO2206 <900522z@gmail.com>
2023-01-31 23:10:06 +0900

    코드에 대한 설명 주석으로 추가

--- core/core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -15,6 +15,7 @@ public class MemberServiceTest {
         //when : 이러한 조건일 때
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
+        // 만약 사항이 있다면, 해당 오류 사항에 대해서 log가 찍히고 문제 없이 작동한다면 초록색 불이 뜨면서 종료 코드까지 실행한다.
 
         //then : 이렇게 된다. (검증하는 로직)
         // Assertions - assertj.core.api를 사용한다.
