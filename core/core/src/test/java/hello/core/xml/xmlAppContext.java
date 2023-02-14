cd73ef1f5254e0a16b38e2ac62b5f187560272e6
JISOO2206 <900522z@gmail.com>
2023-02-14 21:53:04 +0900

    xml 파일 설정시 테스트 코드 구현

---
+++ core/core/src/test/java/hello/core/xml/xmlAppContext.java
@@ -0,0 +1,19 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class xmlAppContext {
+
+    // xml로 설정 시 테스트 코드 구현 (현재 실패 : xml 파일 없음)
+    @Test
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+}
