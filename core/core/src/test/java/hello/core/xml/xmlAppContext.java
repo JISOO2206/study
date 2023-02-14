57b85e98003f646238d34f916f1163f70e33635d
JISOO2206 <900522z@gmail.com>
2023-02-14 22:07:53 +0900

    appConfig.xml 파일 생성 및 구현 후 test 성공여부 체크

--- core/core/src/test/java/hello/core/xml/xmlAppContext.java
+++ core/core/src/test/java/hello/core/xml/xmlAppContext.java
@@ -8,7 +8,7 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class xmlAppContext {
 
-    // xml로 설정 시 테스트 코드 구현 (현재 실패 : xml 파일 없음)
+    // xml로 설정 시 테스트 코드 구현 (현재 성공 : xml 파일 생성 및 설정 후)
     @Test
     void xmlAppContext(){
         ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
