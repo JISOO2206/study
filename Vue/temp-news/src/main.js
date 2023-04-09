9acac67512e52cd9a5a7a4cd71c514ee7abac023
JISOO2206 <900522z@gmail.com>
2023-04-09 19:11:44 +0900

    old project hack-news 제거, new project 생성

--- Vue/temp-news/src/main.js
+++ Vue/temp-news/src/main.js
@@ -3,13 +3,6 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-// let App = {
-//   template: '<div>app</div>'
-// }
-
 new Vue({
   render: h => h(App),
-  // component: {
-  //   'app': App
-  // }
 }).$mount('#app')
